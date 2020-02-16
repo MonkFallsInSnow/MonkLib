6c4e2582615208679fc65c32b9607985b4dcc87e
Conrad <conrad.lewin@gmail.com>
2020-02-16 11:15:41 -0500

    trying to add a generic interface for the gene archrive. doesn't really work...

---
+++ MonkLib/Neat/Interfaces/IArchive.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonkLib.Neat.Interfaces
+{
+    public interface IArchive
+    {
+        void CreateRecord(ConnectionGene connectionData, NodeGene nodeData = null);
+        object GetRecord(object key);
+        bool HasRecord(object key);
+        bool HasActiveRecord(object key);
+    }
+}
