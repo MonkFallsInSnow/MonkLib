fe1e45509cd07ae773a504814ac43d4d0cc8926f
Conrad <conrad.lewin@gmail.com>
2020-02-17 21:16:25 -0500

    forgot to commit constants

--- MonkLib/Neat/Constants.cs
+++ MonkLib/Neat/Constants.cs
@@ -3,6 +3,9 @@
     public static class Constants
     {
         public const double COMPATIBILITY_THRESHOLD = 1;
+        public const double EXCESS_SCALAR = 1;
+        public const double DISJOINT_SCALAR = 1;
+        public const double WEIGHT_SCALAR = 1;
 
         public enum NodeType
         {
